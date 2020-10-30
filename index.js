f0e58ae5440fbac325cbe07c15ca1f22275cf322
SyafiqEXP <73509872+SyafiqEXP@users.noreply.github.com>
2020-10-30 17:19:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.listen(3000,() => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'})); // limit receiving data, put some option to control
 
-app.post('/api', (request, response) => {
+app.post('https://syafiqexp.github.io/api', (request, response) => {
   console.log('I got a request !');
   console.log(request.body); // to receive from the client
   const data = request.body;
